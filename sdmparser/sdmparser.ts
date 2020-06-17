1317c98a98844ded3e8956e57a1229e40f2190ed
hikalium <hikalium@hikalium.com>
2020-06-17 22:44:13 +0900

    Impl ExtractSDMInstrIndex

--- sdmparser/sdmparser.ts
+++ sdmparser/sdmparser.ts
@@ -19,6 +19,7 @@ interface SDMText {
   text?: string;
   attr: any;
   i?: string;
+  a?: {text: string, attr: {href: string}};
 }
 
 interface SDMPage {
@@ -44,11 +45,81 @@ function ExtractSDMDataAttr(filepath: string, firstPage: SDMPage): SDMDataAttr {
   return result;
 }
 
+function ExpandMnemonic(title: string): string[] {
+  const suffixList = ['8', '16', '32', '64', 'B', 'W', 'D', 'Q'];
+  const commaSeparated = title.split(',');
+  let ops = [];
+  for (const s of commaSeparated) {
+    const slashSeparated = s.split('/');
+    if (slashSeparated.length < 2 || !suffixList.includes(slashSeparated[1])) {
+      ops = ops.concat(slashSeparated);
+      continue;
+    }
+    // Adjustment logic for MOVDQU,VMOVDQU8/16/32/64, etc...
+    ops.push(slashSeparated[0]);
+    let base = slashSeparated[0];
+    for (const suffix of suffixList) {
+      if (!base.endsWith(suffix)) continue;
+      base = base.substr(0, base.length - suffix.length);
+    }
+    for (let i = 1; i < slashSeparated.length; i++) {
+      ops.push(base + slashSeparated[i]);
+    }
+  }
+  return ops.map((e) => e.trim());
+}
+
+function ExpandMnemonicTest() {
+  assert.deepEqual(
+      ExpandMnemonic('MOVDQU,VMOVDQU8/16/32/64'),
+      ['MOVDQU', 'VMOVDQU8', 'VMOVDQU16', 'VMOVDQU32', 'VMOVDQU64']);
+  assert.deepEqual(
+      ExpandMnemonic('MOVDQA,VMOVDQA32/64'),
+      ['MOVDQA', 'VMOVDQA32', 'VMOVDQA64']);
+  assert.deepEqual(
+      ExpandMnemonic('MOVS/MOVSB/MOVSW/MOVSD/MOVSQ'),
+      ['MOVS', 'MOVSB', 'MOVSW', 'MOVSD', 'MOVSQ']);
+  assert.deepEqual(
+      ExpandMnemonic('VPBROADCASTB/W/D/Q'),
+      ['VPBROADCASTB', 'VPBROADCASTW', 'VPBROADCASTD', 'VPBROADCASTQ']);
+  assert.deepEqual(ExpandMnemonic(' XTEST '), ['XTEST']);
+}
+
+interface SDMInstrIndex {
+  mnemonics: string[];
+  physical_page: number;
+}
+
+function ExtractSDMInstrIndex(sdmPages: SDMPage[]): SDMInstrIndex[] {
+  const index =
+      sdmPages.map((e) => e.text)
+          .flat()
+          .filter((e) => e.a !== undefined)
+          .map((e) => e.a)
+          .filter((e) => e.text && e.text.toString().indexOf('—') != -1)
+          .map((e): SDMInstrIndex => {
+            const title = e.text.toString().split('.')[0].split('—')[0];
+            return {
+              mnemonics: ExpandMnemonic(title),
+              physical_page: parseInt(e.attr.href.split('#')[1]),
+            };
+          });
+  const instrIndex = [];
+  let lastPage = 0;
+  for (const e of index) {
+    if (lastPage > e.physical_page) break;
+    lastPage = e.physical_page;
+    instrIndex.push(e);
+  }
+  return instrIndex;
+}
+
 (() => {
+  ExpandMnemonicTest();
   const filepath = 'pdf/325383-sdm-vol-2abcd.xml'
   const data = fs.readFileSync(filepath, 'utf-8');
   const options = {
-    attributeNamePrefix: '@_',
+    attributeNamePrefix: '',
     attrNodeName: 'attr',  // default is 'false'
     textNodeName: 'text',
     ignoreAttributes: false,
@@ -74,5 +145,7 @@ function ExtractSDMDataAttr(filepath: string, firstPage: SDMPage): SDMDataAttr {
   const sdm = parser.parse(data, options);
   assert.ok(sdm.pdf2xml.page);
   const sdmPages: SDMPage[] = <SDMPage[]>sdm.pdf2xml.page;
-  console.log(ExtractSDMDataAttr(filepath, sdm.pdf2xml.page[0]));
+  console.log(ExtractSDMDataAttr(filepath, sdmPages[0]));
+  const instrIndex: SDMInstrIndex[] = ExtractSDMInstrIndex(sdmPages);
+  console.log(JSON.stringify(instrIndex, null, ' '));
 })();
